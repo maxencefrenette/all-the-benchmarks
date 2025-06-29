a9738c2d36741e71cb372a77128c8f96c533b8c5
Maxence Frenette <maxence.frenette@gmail.com>
2025-06-29 11:33:35 -0700

    Add basic testing setup with vitest

---
+++ lib/__tests__/data-loader.test.ts
@@ -0,0 +1,15 @@
+import { transformToTableData, type LLMData } from "../data-loader"
+import { expect, test } from "vitest"
+
+// Simple unit test for transformToTableData
+
+test("transformToTableData converts LLMData objects to table rows", () => {
+  const llms: LLMData[] = [
+    { model: "Foo", provider: "Bar", benchmarks: {}, averageScore: 42 },
+  ]
+
+  const rows = transformToTableData(llms)
+  expect(rows).toEqual([
+    { id: "foo", model: "Foo", provider: "Bar", averageScore: 42 },
+  ])
+})
