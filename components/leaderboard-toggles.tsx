bfa70a18d8d10807be041cb7f73663a114810e22
Maxence Frenette <maxence.frenette@gmail.com>
2025-07-13 11:00:24 -0700

    style: add margin below leaderboard toggles

--- components/leaderboard-toggles.tsx
+++ components/leaderboard-toggles.tsx
@@ -24,7 +24,7 @@ export default function LeaderboardToggles() {
   }
 
   return (
-    <div className="flex items-center justify-center gap-2">
+    <div className="flex items-center justify-center gap-2 mb-4">
       <div className="flex items-center gap-2">
         <Switch
           id="deprecated-toggle"
